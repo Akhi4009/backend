a6f0e4dd740224331891af7ff5dac8532c02157c
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-02-05 09:07:36 +0530

    morgan

--- starter/index.js
+++ starter/index.js
@@ -1,14 +1,26 @@
 const fs=require("fs")
 const express=require("express")
+const morgan=require("morgan")
 
 const app=express()
+
+//// MiddleWare 
+
+app.use(morgan("common"))
+
+
 app.use(express.json())
+app.use((req,res,next)=>{
+    req.requestTime=new Date().toISOString()
+    next()
+})
 
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTour=(req,res)=>{
     res.status(200).json({
         status:"success",
+        requestTime:req.requestTime,
         results:tours.length,
         data:{
             tours
